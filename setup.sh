d0909c657204455a5c6a66ec27218906e2d6b9ea
NextGen DevSecOps & GenAI <smilemukul2005@gmail.com>
2026-07-22 16:04:32 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -7,7 +7,7 @@ if ! command -v ollama >/dev/null 2>&1; then
     echo
     echo "Installing Ollama..."
 
-    curl -fsSL https://ollama.com/install.sh | sh
+    curl -LO https://ollama.com/download/ollama-linux-amd64.tar.zst && file ollama-linux-amd64.tar.zst
 
 fi
 
